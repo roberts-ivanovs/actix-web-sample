e52935bca72d9aac021daa5aa8ea143a24979597
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-10-23 22:04:04 +0300

    created trasnaction

--- src/views/turnirs.rs
+++ src/views/turnirs.rs
@@ -90,124 +90,107 @@ pub async fn add_turnirs_post(
 
     let mut conn = DB_WRAPPER.get_conn();
 
+    let transaction_query = "START TRANSACTION;";
+    // Crash explicitly if failed to start transaction
+    let transaction_query_result = conn.query_drop(transaction_query).unwrap();
+    let final_result = perform_transaction_actions(conn, query_data);
+
+    match final_result {
+        Ok(mut conn_ret) => {
+            let transaction_query_rollback = "COMMIT;";
+            conn_ret.query_drop(transaction_query_rollback).unwrap();
+
+            let base_path = extract_base_path(req.path(), "add/");
+            return Ok(redirect_to(base_path));
+        },
+        Err((error, mut conn_ret)) => {
+            let transaction_query_rollback = "ROLLBACK;";
+            conn_ret.query_drop(transaction_query_rollback).unwrap();
+            return Ok(redirect_to(req.path()));}
+    };
+}
+
+
+fn perform_transaction_actions(mut conn: PooledConn, query_data: TurniraIzveide ) -> Result<PooledConn, (mysql::Error, PooledConn)> {
+
     let empty_turnirs = Turnirs {
         id: 0,
         turnira_datums: Some(query_data.datetime),
         turnira_nosaukums: Some(query_data.name)
     };
+
     let created_turnirs = Turnirs::create(&mut conn, empty_turnirs.clone());
+
+    match created_turnirs {
+        Err(err) => {
+            return Err((err, conn));
+        },
+        Ok(_) => {}
+    }
+
     let turnirs_speletaji = query_data.players;
     let parks_trase = query_data.tracks;
 
-    let transaction_query = "START TRANSACTION;";
-    let transaction_query_result = conn.query_drop(transaction_query);
-
-    match transaction_query_result {
-        Ok(_) => {},
-        Err(_) => {
-            let transaction_query_rollback = "ROLLBACK;";
-            conn.query_drop(transaction_query_rollback);
-            return Ok(redirect_to(req.path()));
-        }
-    }
 
     let query_select = format!("SELECT id from Turnirs ORDER BY id desc LIMIT 1");
-    let turnirs_id = conn
-            .query_first::<(
-                u32
-            ), String>(query_select);
+    let turnirs_id = conn.query_first::<u32, String>(query_select);
 
-    match &turnirs_id {
-        Ok(_) => {},
-        Err(_) => {
-            let transaction_query_rollback = "ROLLBACK;";
-            conn.query_drop(transaction_query_rollback);
-            return Ok(redirect_to(req.path()));
-        }
-    }
+    match turnirs_id {
+        Ok(turnirs_id) => {
+            let mut turnirs_speletajs_saraksts: Vec<u32> = vec![];
+            let unwrapper_turnirs_id = turnirs_id.unwrap();
+            for speletajs in &turnirs_speletaji{
+                let insert_query = format!("INSERT INTO TurnirsSpeletajs(turnirs_FK, speletajs_FK) values({}, {})", &unwrapper_turnirs_id, speletajs);
+                let insert_query_result = conn.query_drop(insert_query);
 
-
-    let mut turnirs_speletajs_saraksts: Vec<u32> = vec![];
-    let unwrapper_turnirs_id = turnirs_id.unwrap().unwrap();
-    for speletajs in &turnirs_speletaji{
-        let insertQuery = format!("INSERT INTO TurnirsSpeletajs(turnirs_FK, speletajs_FK) values({}, {})", &unwrapper_turnirs_id, speletajs);
-        let insert_query_result = conn.query_drop(insertQuery);
-        match insert_query_result {
-            Ok(_) => {},
-            Err(_) => {
-                let transaction_query_rollback = "ROLLBACK;";
-                conn.query_drop(transaction_query_rollback);
-                return Ok(redirect_to(req.path()));
-            }
-        }
-
-        let query_turnirs_speletajs_select = format!("SELECT id FROM TurnirsSpeletajs ORDER BY id desc LIMIT 1");
-        let turnirs_speletajs_id_result = conn
-        .query_first::<(
-            u32
-        ), String>(query_turnirs_speletajs_select);
-
-        match &turnirs_speletajs_id_result {
-            Ok(_) => {},
-            Err(_) => {
-                let transaction_query_rollback = "ROLLBACK;";
-                conn.query_drop(transaction_query_rollback);
-                return Ok(redirect_to(req.path()));
-            }
-        }
-        turnirs_speletajs_saraksts.push(turnirs_speletajs_id_result.unwrap().unwrap().clone());
-
-    }
-
-    let query_trase_grozs_select = format!("SELECT id FROM TraseGrozs WHERE trase_FK = {} ", parks_trase);
-    let trase_grozs_result: Result<Vec<u32>, mysql::Error> = conn.query_map(query_trase_grozs_select, |trase_grozs_id| trase_grozs_id);
-
-    match trase_grozs_result {
-        Ok(_) => {},
-        Err(_) => {
-            let transaction_query_rollback = "ROLLBACK;";
-            conn.query_drop(transaction_query_rollback);
-            return Ok(redirect_to(req.path()));
-        }
-    }
-
-
-    for speletajs in turnirs_speletajs_saraksts{
-        for id in &trase_grozs_result.unwrap(){
-            let query_add_results = format!("INSERT INTO Rezultats(trase_grozs_FK, TurnirsSpeletajs_FK) values({}, {})", id, speletajs);
-            let query_add = conn.query_drop(query_add_results);
-            match query_add {
-                Ok(_) => {},
-                Err(_) => {
-                    let transaction_query_rollback = "ROLLBACK;";
-                conn.query_drop(transaction_query_rollback);
-                return Ok(redirect_to(req.path()));
+                match insert_query_result {
+                    Ok(_) => {
+                        let query_turnirs_speletajs_select = format!("SELECT id FROM TurnirsSpeletajs ORDER BY id desc LIMIT 1");
+                        let turnirs_speletajs_id_result = conn.query_first::<u32, String>(query_turnirs_speletajs_select);
+                        match turnirs_speletajs_id_result {
+                            Ok(turnirs_speletajs_id_result) => {
+                                turnirs_speletajs_saraksts.push(turnirs_speletajs_id_result.unwrap().clone());
+                            }
+                            Err(err) => {
+                                return Err((err, conn));
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        return Err((err, conn));
+                    }
                 }
             }
+
+            let query_trase_grozs_select = format!("SELECT id FROM TraseGrozs WHERE trase_FK = {} ", parks_trase);
+            let trase_grozs_result: Result<Vec<u32>, mysql::Error> = conn.query_map(query_trase_grozs_select, |trase_grozs_id| trase_grozs_id);
+            let trase_grozs_result = trase_grozs_result;
+
+            match trase_grozs_result {
+                Ok(trase_grozs_result) => {
+                    for speletajs in &turnirs_speletajs_saraksts{
+                        for id in &trase_grozs_result{
+                            let query_add_results = format!("INSERT INTO Rezultats(trase_grozs_FK, TurnirsSpeletajs_FK) values({}, {})", id, speletajs);
+                            let res = conn.query_drop(query_add_results);
+                            match res {
+                                Ok(_) => {}
+                                Err(err) => {
+                                    return Err((err, conn));
+                                }
+                            }
+                        }
+                    }
+                }
+                Err(err) =>  {
+                return Err((err, conn));
+                }
+            }
+
+        },
+        Err(err) => {
+            return Err((err, conn));
         }
     }
 
-    let transaction_query_commit = "COMMIT;";
-    let transaction_query_commit_result = conn.query_drop(transaction_query_commit);
-
-    match transaction_query_commit_result {
-        Ok(_) => {},
-        Err(_) => {
-            let transaction_query_rollback = "ROLLBACK;";
-            conn.query_drop(transaction_query_rollback);
-            return Ok(redirect_to(req.path()));
-        }
-    }
-
-
-    let base_path = extract_base_path(req.path(), "add/");
-    match created_turnirs {
-        Ok(_) => {
-            Ok(redirect_to(base_path))
-        }
-        Err(_) => {
-            Ok(redirect_to(req.path()))
-        }
-    }
-
+    Ok(conn)
 }
